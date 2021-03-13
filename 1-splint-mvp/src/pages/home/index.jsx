1089ceff4dd3164834ae9b9823e77b0475e24cb6
yusokk <13chakbo@naver.com>
2021-03-13 22:41:07 +0900

    frontend practice

--- 1-splint-mvp/src/pages/home/index.jsx
+++ 1-splint-mvp/src/pages/home/index.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
-const dummy = [
-  {
-    title: '홍지의 그림방',
-    id: 1,
-  },
-  {
-    title: '유석이의 그림방',
-    id: 2,
-  },
-];
+const axios = require('axios');
+
+// const dummy = axios.get('/');
+const dummy = axios.get('localhost:8080/');
 const Home = () => {
   return (
     <div>
       <p>Home component</p>
       {dummy.map((data, index) => {
         return (
-          <div key={index}>
-            <div>Title: {data.title}</div>
-            <div>Id: {data.id}</div>
-            <br />
+          // <Link to='/'>Home</Link>
+          <div key={index} style={{ margin_bottom: '20px' }}>
+            <Link to='/draw'>
+              <div style={{ border: '2px solid', display: 'inline-block' }}>
+                Title: {data.title}
+                Id: {data.roomId}
+              </div>
+            </Link>
           </div>
         );
       })}
