89d306be7bcc6096de6c3e8bd7468dddb27b5cb5
jeongmin94 <jeongmin94@github.com>
2021-03-19 17:32:55 +0900

    B | test websocket connection

---
+++ 1-splint-mvp/frontend/src/pages/routes/CreateRoom.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { v1 as uuid } from "uuid";
+
+const CreateRoom = (props) => {
+    function create() {
+        console.log(props);
+        const id = uuid();
+        props.history.push(`/rtc/${id}`);
+    }
+
+    return (
+        <button onClick={create}>Create room</button>
+    );
+};
+
+export default CreateRoom;
