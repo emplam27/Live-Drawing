d2dd9bcef626046c1f42bf258486ab1dcf9d2520
yusokk <13chakbo@naver.com>
2021-03-19 20:59:44 +0900

    U | socket test complete

--- 1-splint-mvp/frontend/src/App.js
+++ 1-splint-mvp/frontend/src/App.js
@@ -1,17 +1,19 @@
-import React from "react"
-import { Link, Route } from "react-router-dom"
-import Home from "./pages/home"
-import Draw from "./pages/draw"
-import logo from "./logo.svg"
-import "./App.css"
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Draw from './pages/draw'
+import logo from './logo.svg'
+import './App.css'
+import CreateRoom from './pages/routes/CreateRoom.jsx'
+import Room from './pages/routes/Room.jsx'
 
 function App() {
   return (
     <div className="App">
       <main>
         <div>
-          <Route path="/" component={Home} exact />
-          <Route path="/draw/:roomKey" component={Draw} exact />
+          <Route path="/" component={CreateRoom} exact />
+          <Route path="/rtc/:roomKey" component={Room} exact />
         </div>
       </main>
     </div>
