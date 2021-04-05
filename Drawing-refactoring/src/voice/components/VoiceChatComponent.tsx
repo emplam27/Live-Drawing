f7660ab6fc1efc67a4b7c6e6f043a3e666cde8b8
2014100890 <31299657+2014100890@users.noreply.github.com>
2021-04-06 04:21:29 +0900

    B | connection error(mic, speaker) fixed

--- Drawing-refactoring/src/voice/components/VoiceChatComponent.tsx
+++ Drawing-refactoring/src/voice/components/VoiceChatComponent.tsx
@@ -16,26 +16,19 @@ const {
 } = require('agora-access-token');
 
 export function VoiceChatComponent(props: Rtc) {
-  // 토큰 생성 및 방 참여
-  const [speaker, setSpeaker] = useState<boolean | null>(false);
-  const [mic, setMic] = useState<boolean | null>(false);
-
+  const [speaker, setSpeaker] = useState<boolean | null>(null);
+  const [mic, setMic] = useState<boolean | null>(null);
   const [toggleSpeakerSignal, setToggleSpeakerSignal] = useState<
     boolean | null
   >(null);
   const [toggleMicSignal, setToggleMicSignal] = useState<boolean | null>(null);
-
   const [
     localAudioTrack,
     setLocalAudiotrack,
   ] = useState<ILocalAudioTrack | null>(null);
-  const [remoteAudioTracks, setRemoteAudioTracks] = useState<
-    IRemoteAudioTrack[]
-  >([]);
-
   const [remotesignal, setRemoteSignal] = useState<boolean | null>(null);
   const [audiosignal, setAudiosignal] = useState<number | null>(null);
-  const [clientsignal, setClientSignal] = useState<boolean | null>(null);
+  const [newUserSignal, setNewUserSignal] = useState<number | null>(null);
   const [client, setClient] = useState<IAgoraRTCClient | null>(
     AgoraRTC.createClient({
       codec: 'vp8',
@@ -48,59 +41,37 @@ export function VoiceChatComponent(props: Rtc) {
     if (!localAudioTrack || !client) return;
     client.publish([localAudioTrack]);
     client.on('user-published', async (user, mediaType) => {
-      await client.subscribe(user, mediaType);
-      console.log('!!!!!!!새로 들어왔어요!!!!!!!!!!', remoteAudioTracks);
-      console.log('!!!!!!!클라이언트의 리모트 유저스다!!!!!!!!!!');
-      client.remoteUsers.forEach((remoteUser) => {
-        console.log('================1==', speaker, mic, remoteUser.audioTrack);
-        if (remoteUser.audioTrack) remoteUser.audioTrack.play();
-        console.log(
-          '================2===',
-          speaker,
-          mic,
-          remoteUser.audioTrack,
-        );
-        if (speaker === false) remoteUser.audioTrack?.setVolume(0);
-        // 스피커가 false면 volume 0 으로
-      });
-      // 마이크가 false면 unpublish
-      if (mic === false) client.unpublish([localAudioTrack]);
+      if (user && mediaType === 'audio') {
+        console.log('**********', user, mediaType);
+        await client.subscribe(user, mediaType);
+        console.log('!!!!!!!클라이언트의 리모트 유저스다!!!!!!!!!!');
+        setNewUserSignal(new Date().getTime());
+      }
     });
   }, [localAudioTrack]);
 
   function toggleSpeaker(speaker: boolean | null) {
     setToggleSpeakerSignal(speaker);
-    console.log('!!!!!!!!!!!!!speaker!!!!!!!!!!!!!', speaker);
   }
 
   function toggleMic(mic: boolean | null) {
     setToggleMicSignal(mic);
-    console.log('!!!!!!!!!!!!!mic!!!!!!!!!!!!!!', mic);
   }
-  useEffect(() => {
-    remoteAudioTracks.forEach((remoteAudioTrack) => {
-      if (speaker === true) {
-        remoteAudioTrack.setVolume(100);
-      } else {
-        remoteAudioTrack.setVolume(0);
-      }
-    });
-  }, [remoteAudioTracks]);
 
   /* 스피커 on/off */
   useEffect(() => {
-    if (!remoteAudioTracks || toggleSpeakerSignal === null) return;
-    if (speaker === true || speaker == null) {
-      remoteAudioTracks.forEach((remoteAudioTrack) => {
-        // remoteAudioTrack.setVolume(0);
-        remoteAudioTrack.play();
-        // stop을 풀 때 다른 사람들의 이벤트를 듣게해야해
+    // 맨 처음 toggle값이 널일 때 여기로 들어가게 된다.
+    console.log('==== check toggle speaker signal', toggleSpeakerSignal);
+    if (!client || toggleSpeakerSignal === null) return;
+    if (toggleSpeakerSignal === false) {
+      client.remoteUsers.forEach((user) => {
+        user.audioTrack?.setVolume(0);
       });
+
       setSpeaker(false);
-    } else if (speaker === false) {
-      remoteAudioTracks.forEach((remoteAudioTrack) => {
-        // remoteAudioTrack.setVolume(100);
-        remoteAudioTrack.stop();
+    } else if (toggleSpeakerSignal === true) {
+      client.remoteUsers.forEach((user) => {
+        user.audioTrack?.setVolume(100);
       });
       setSpeaker(true);
     }
@@ -108,17 +79,32 @@ export function VoiceChatComponent(props: Rtc) {
 
   /* 마이크 조절 */
   useEffect(() => {
-    if (!client || !localAudioTrack || toggleMicSignal === null) return;
-    if (mic === true || mic == null) {
+    if (!client || !localAudioTrack) return;
+    if (toggleMicSignal === false || toggleMicSignal === null) {
       client.unpublish([localAudioTrack]);
       // localAudioTrack.setEnabled(false);
       setMic(false);
-    } else if (mic === false) {
+    } else if (toggleMicSignal === true) {
       // localAudioTrack.setEnabled(true);
       client.publish([localAudioTrack]);
       setMic(true);
     }
   }, [toggleMicSignal]);
+
+  useEffect(() => {
+    if (!client || !localAudioTrack || newUserSignal === null) return;
+    client.remoteUsers.forEach((remoteUser) => {
+      if (remoteUser.audioTrack) remoteUser.audioTrack.play();
+      if (speaker === false) remoteUser.audioTrack?.setVolume(0);
+    });
+    if (mic === false) client.unpublish([localAudioTrack]);
+    console.log('!!!!!!!새로운 친구가 들어왔어요!!!!!!!!!!');
+    console.log('친구들', client.remoteUsers);
+    console.log('내 스피커 상황', speaker);
+    console.log('내 마이크 상황', mic);
+    console.log('토글 스피커 시그널', toggleSpeakerSignal);
+    console.log('토글 마이크 시그널', toggleMicSignal);
+  }, [newUserSignal]);
 
   useEffect(() => {
     const appID = `${process.env.REACT_APP_AGORA_APP_ID}`;
@@ -143,7 +129,9 @@ export function VoiceChatComponent(props: Rtc) {
       if (!client) return;
       setUid(await client.join(appID, channelName, tokenA, null));
       setLocalAudiotrack(await AgoraRTC.createMicrophoneAudioTrack());
+      setToggleSpeakerSignal(false);
       setSpeaker(false);
+      setToggleMicSignal(false);
       setMic(false);
     }
     startBasicCall();
@@ -155,27 +143,30 @@ export function VoiceChatComponent(props: Rtc) {
         <p className={'icon-link center'}>
           <i
             className={'ri-2x ri-volume-up-line'}
-            onClick={() => toggleSpeaker(speaker)}
+            onClick={() => toggleSpeaker(!speaker)}
           ></i>
         </p>
       ) : (
         <p className={'icon-link center'}>
           <i
             className={'ri-2x ri-volume-mute-line'}
-            onClick={() => toggleSpeaker(speaker)}
+            onClick={() => toggleSpeaker(!speaker)}
           ></i>
         </p>
       )}
 
       {mic ? (
         <p className={'icon-link center'}>
-          <i className={'ri-2x ri-mic-line'} onClick={() => toggleMic(mic)}></i>
+          <i
+            className={'ri-2x ri-mic-line'}
+            onClick={() => toggleMic(!mic)}
+          ></i>
         </p>
       ) : (
         <p className={'icon-link center'}>
           <i
             className={'ri-2x ri-mic-off-line'}
-            onClick={() => toggleMic(mic)}
+            onClick={() => toggleMic(!mic)}
           ></i>
         </p>
       )}
