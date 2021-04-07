9101cd554f1e01e6d30a3b4f81b6019e4c404cbe
2014100890 <31299657+2014100890@users.noreply.github.com>
2021-04-07 20:46:28 +0900

    U | mainpage update

--- Home_page/src/pages/home/index.tsx
+++ Home_page/src/pages/home/index.tsx
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useCustomState } from '../../context';
 import { RoomListComponent } from './components/room-list';
-import './home.css';
+// import './home.css';
 
 export function HomeComponent() {
   const userState = useCustomState();
-  
+
   return (
     <div className='container'>
-      <div className='lecture-container'>
+      <div className='lecture-container w-screen'>
         <div className='titleAndButton'>
-          <div className='titleContainer'>
-            <div className='title'>
-              라이브 강의 &nbsp;
-              <i className='ri-live-fill'></i>
+          <div className='titleContainer pt-20'>
+            <div className='title pb- text-center text-3xl font-bold text-blue-400'>
+              언제, 어디서든 그림을 배워봅시다
+              {/* 라이브 강의 &nbsp;
+              <i className='ri-live-fill'></i> */}
+            </div>
+            <div>
+              <p className='pt-3 text-center text-2xl font-normal text-gray-400 pb-12'>
+                선생님이 쉽게 알려주실거에요. 걱정은 넣어둬~~
+              </p>
             </div>
           </div>
         </div>
-        <RoomListComponent></RoomListComponent>
-      </div>
-      <div className='lecture-container'>
-        <div className='title'>드로잉 강의</div>
-        <RoomListComponent></RoomListComponent>
+        <div className='mx-52'>
+          <RoomListComponent></RoomListComponent>
+        </div>
       </div>
     </div>
   );
