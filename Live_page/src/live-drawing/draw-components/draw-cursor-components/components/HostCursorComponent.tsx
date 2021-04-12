dd3234cdfee5fda3a106ba0f9d14a11c48ab2438
yusokk <13chakbo@naver.com>
2021-04-12 22:34:08 +0900

    commit to merge

--- Live_page/src/live-drawing/draw-components/draw-cursor-components/components/HostCursorComponent.tsx
+++ Live_page/src/live-drawing/draw-components/draw-cursor-components/components/HostCursorComponent.tsx
@@ -5,8 +5,8 @@ export function HostCursorComponent(props: HostCursorComponentProps) {
   return (
     <div
       style={{
-        left: props.point ? props.point.x + 'px' : 0,
-        bottom: props.point ? props.point.y + 'px' : 0,
+        left: props.point ? `${props.point.x}px` : 0,
+        bottom: props.point ? `${props.point.y}px` : 0,
         width: '10px',
         height: '10px',
         border: '1px solid black',
